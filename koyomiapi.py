d895f9586a3e90f6b4e4236d330e213ccc36b05e
hasegawa <hasegawa@10.211.55.2>
2013-01-23 17:56:13 +0900

    start!

---
+++ koyomiapi.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="hasegawa"
+__date__ ="$2013/01/23 17:43:08$"
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/24sekki", methods=["GET"])
+def sekki24():
+    return 'aho'
+
+app.run()
