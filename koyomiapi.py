23992fd7ec28ab5b7f90872b530d8c0e45bbedf0
hasegawa <hasegawa@10.211.55.2>
2013-01-24 10:25:56 +0900

    year,indexパラメータ追加

--- koyomiapi.py
+++ koyomiapi.py
@@ -5,13 +5,56 @@
 __author__="hasegawa"
 __date__ ="$2013/01/23 17:43:08$"
 
-from flask import Flask
+from flask import Flask, request, g
+import datetime
 
 app = Flask(__name__)
 
+@app.before_request
+def getToday():
+    g.date = datetime.datetime.today()
+
 @app.route("/24sekki", methods=["GET"])
 def sekki24():
-    return 'aho'
+    
+    year = g.date.year
+    index = []
+    
+    #
+    # 引数チェック
+    #
+    
+    # year
+    try:
+        year = int(request.args["year"])
+    
+    except ValueError:
+        return invalidParameter()
+    except:
+        pass
+    
+    # このAPIは1901 - 2099までしか対応していない
+    if year < 1901 or year > 2099:
+        return invalidParameter()
+    
+    # no
+    try:
+        for i in request.args["index"].split(","):
+            index.append(int(i))
+    
+    except ValueError:
+        return invalidParameter()
+    except:
+        pass
+    
+    return "date:{0}/{1}/{2} {3}:{4} year={5}".format(g.date.year, g.date.month,g.date.day, g.date.hour, g.date.minute,str(year))
+        
+
+
+
+def invalidParameter(code=400):
+    return "Invalid parameter", code
 
 if __name__ == "__main__":
-  app.run()
+    app.debug = True
+    app.run()
